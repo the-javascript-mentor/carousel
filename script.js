b4c9f8d61ef253bb1232f9ef5d74dbbadbb4f4d4
Bence A. Tóth <tothab@gmail.com>
2021-11-25 18:11:42 +0100

    Made carousel responsive, added edge detection

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-document.getElementById("product-list").innerHTML = products
+const productWidth = 240;
+const productListNode = document.getElementById("product-list");
+
+const getNumberOfVisibleCarouselItems = () => {
+  let items = 4;
+  if (window.matchMedia("(max-width: 1280px)").matches) {
+    items = 3;
+  }
+  if (window.matchMedia("(max-width: 920px)").matches) {
+    items = 2;
+  }
+  return items;
+};
+
+window.addEventListener("resize", () => {
+  console.log(getNumberOfVisibleCarouselItems());
+});
+
+productListNode.innerHTML = products
   .map(
     (product) => `
       <li class="product">
@@ -14,16 +32,30 @@ document.getElementById("product-list").innerHTML = products
   )
   .join("");
 
+productListNode.addEventListener("scroll", (event) => {
+  if (productListNode.scrollLeft % productWidth === 0) {
+    if (productListNode.scrollLeft === 0) {
+      console.log("Hit the left edge");
+    }
+    if (
+      productListNode.scrollLeft ===
+      productWidth * (products.length - getNumberOfVisibleCarouselItems())
+    ) {
+      console.log("Hit the right edge");
+    }
+  }
+});
+
 document.getElementById("right").addEventListener("click", () => {
   document.getElementById("product-list").scrollBy({
-    left: 240,
+    left: productWidth,
     behavior: "smooth",
   });
 });
 
 document.getElementById("left").addEventListener("click", () => {
   document.getElementById("product-list").scrollBy({
-    left: -240,
+    left: -productWidth,
     behavior: "smooth",
   });
 });
